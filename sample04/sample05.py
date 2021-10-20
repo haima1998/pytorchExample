451a0c0927331de9fe99ac7c548ab1fa0056930e
ying.zhuo <ying.zhuo@rokid.com>
2021-10-20 15:10:07 +0800

    sample05.py

---
+++ sample04/sample05.py
@@ -0,0 +1,118 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from tensorboardX import SummaryWriter
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.utils as vutils
+
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class CNNNet(nn.Module):
+    def __init__(self):
+        super(CNNNet,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3,out_channels=16,kernel_size=5,stride=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2,stride=2)
+        self.conv2 = nn.Conv2d(in_channels=16,out_channels=36,kernel_size=3,stride=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(1296,128)
+        self.fc2 = nn.Linear(128,10)      
+
+    def forward(self,x):
+        x=self.pool1(F.relu(self.conv1(x)))
+        x=self.pool2(F.relu(self.conv2(x)))
+        #print(x.shape)
+        x=x.view(-1,36*6*6)
+        x=F.relu(self.fc2(F.relu(self.fc1(x))))
+        return x
+
+net = CNNNet()
+net=net.to(device)
+
+LR=0.001
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9)
+
+
+#初始化数据
+for m in net.modules():
+    if isinstance(m,nn.Conv2d):
+        nn.init.normal_(m.weight)
+        nn.init.xavier_normal_(m.weight)
+        nn.init.kaiming_normal_(m.weight)#卷积层参数初始化
+        nn.init.constant_(m.bias, 0)
+    elif isinstance(m,nn.Linear):
+        nn.init.normal_(m.weight)#全连接层参数初始化
+
+#训练模型
+for epoch in range(2):  
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # 获取训练数据
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        # 权重参数梯度清零
+        optimizer.zero_grad()
+
+        # 正向及反向传播
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # 显示损失值
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %(epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+writer = SummaryWriter(log_dir='logs',comment='feature map')
+for i, data in enumerate(trainloader, 0):
+        # 获取训练数据
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        x=inputs[0].unsqueeze(0)
+        break
+
+img_grid = vutils.make_grid(x, normalize=True, scale_each=True, nrow=2)
+
+net.eval()
+for name, layer in net._modules.items():
+
+    # 为fc层预处理x
+    x = x.view(x.size(0), -1) if "fc" in name else x
+    print(x.size())
+
+    x = layer(x)
+    print(f'{name}')
+
+    # 查看卷积层的特征图
+    if  'layer' in name or 'conv' in name:
+        x1 = x.transpose(0, 1)  # C，B, H, W  ---> B，C, H, W
+        img_grid = vutils.make_grid(x1, normalize=True, scale_each=True, nrow=4)  # normalize进行归一化处理
+        writer.add_image(f'{name}_feature_maps', img_grid, global_step=0)
